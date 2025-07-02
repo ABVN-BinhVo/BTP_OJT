c0f6c2d349ef2ba1d262f612471e61a5c80d7358
ABVN-BinhVo <bvo@abeam.com>
2025-07-02 06:16:18 +0000

    App config

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const express = require('express');
 
 cds.on('bootstrap', app => {
   // Serve static content from webapp folder
-  app.use(express.static('webapp'));
+  app.use(express.static('app'));
   
   // Proxy requests for SAPUI5 resources to the CDN
   app.use('/resources', (req, res) => {
     res.redirect(`https://ui5.sap.com/resources${req.url}`);
   });
+
+
   
   // Alternatively, if you have a specific version you want to use:
   // app.use('/resources', (req, res) => {
